8b04941cb95a0abcd0536dd4c0c1a8549acdad79
Alan Loovees <alanloovees@gmail.com>
2023-05-07 23:43:36 +0530

    added logout option

--- app/javascript/src/apis/auth.js
+++ app/javascript/src/apis/auth.js
@@ -4,9 +4,12 @@ const signup = payload => axios.post("api/v1/signup", payload);
 
 const login = payload => axios.post("api/v1/login", payload);
 
+const logout = () => axios.delete("api/v1/logout");
+
 const authApi = {
   signup,
   login,
+  logout,
 };
 
 export default authApi;
--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { Button, Container, Stack, TextField, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
+import authApi from "apis/auth";
+
 const StyledContent = styled("div")(() => ({
   margin: "auto",
   minHeight: "100vh",
@@ -11,25 +13,47 @@ const StyledContent = styled("div")(() => ({
   flexDirection: "column",
 }));
 
-const Dashboard = () => (
-  <Container maxWidth="sm">
-    <StyledContent>
-      <Stack spacing={2}>
-        <Typography>We grow when you spread the word:</Typography>
-        <TextField label="Email to Refer" name="email" />
-        <Button
-          fullWidth
-          size="large"
-          type="submit"
-          variant="contained"
-          // onClick={handleSubmit}
-        >
-          Refer
-        </Button>
-      </Stack>
-    </StyledContent>
-    ;
-  </Container>
-);
+const Dashboard = () => {
+  const handleLogout = async () => {
+    try {
+      await authApi.logout();
+      localStorage.removeItem("token");
+      window.location.href = "/";
+    } catch (error) {
+      logger.error(error);
+    }
+  };
+
+  return (
+    <Container maxWidth="sm">
+      <StyledContent>
+        <Stack spacing={2}>
+          <Typography>We grow when you spread the word:</Typography>
+          <TextField label="Email to Refer" name="email" />
+          <Button
+            fullWidth
+            size="large"
+            type="submit"
+            variant="contained"
+            // onClick={handleSubmit}
+          >
+            Refer
+          </Button>
+          <Button
+            fullWidth
+            color="error"
+            size="large"
+            type="submit"
+            variant="contained"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </Stack>
+      </StyledContent>
+      ;
+    </Container>
+  );
+};
 
 export default Dashboard;
